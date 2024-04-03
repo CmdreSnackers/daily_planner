1f8cc4aa04857ca194c4ef33cfbb6e1497cee921
snackers <pvt.stiggs@gmail.com>
2024-04-03 11:14:08 +0800

    edit_branch

--- src/pages/PlanView/index.js
+++ src/pages/PlanView/index.js
@@ -1,3 +1,22 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Details from "./details";
+import { Container } from "@mui/material";
+import Nav from "../../components/Nav";
+
 export default function PlanView() {
-  return <>Plan View</>;
+  const { id } = useParams();
+
+  const plans = JSON.parse(localStorage.getItem("plans"));
+
+  const plan = plans.find((p) => p.id === id);
+
+  return (
+    <>
+      <Nav />
+      <Container maxWidth="sm">
+        <Details plan={plan} />
+      </Container>
+    </>
+  );
 }
