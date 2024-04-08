a4fa74bc0319d64bac1a05e73d0f64906fca70c1
CmdreSnackers <103157897+CmdreSnackers@users.noreply.github.com>
2024-04-08 15:42:32 +0800

    Revert "Apr8 adrian"

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import "./styles.css";
+import logo from './logo.svg';
+import './App.css';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import PlanAddNew from "./pages/PlanAddNew";
-import PlanEdit from "./pages/PlanEdit";
-import PlanView from "./pages/PlanView";
-import { SnackbarProvider } from "material-ui-snackbar-provider";
-import CustomSnackbar from "./components/CustomSnackbar";
-
-export default function App() {
+function App() {
   return (
-    <SnackbarProvider
-      SnackbarComponent={CustomSnackbar}
-      SnackbarProps={{ autoHideDuration: 4000 }}
-    >
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/plan/:id" element={<PlanView />} />
-          <Route path="/plan/:id/edit" element={<PlanEdit />} />
-          <Route path="/new" element={<PlanAddNew />} />
-        </Routes>
-      </BrowserRouter>
-    </SnackbarProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
